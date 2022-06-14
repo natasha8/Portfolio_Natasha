0363f92c2ba200aa6d7a9368166bfd29e6032538
natasha8 <nat.parisella@gmail.com>
2022-06-14 20:26:03 +0200

    fixed for deploy

--- src/Components/Portfolio.js
+++ src/Components/Portfolio.js
@@ -1,54 +1,54 @@
 import React from "react";
 
 const Portfolio = ({ data }) => {
-	if (data) {
-		var projects = data.projects.map(function (projects) {
-			var projectImage = "images/portfolio/" + projects.image;
-			return (
-				<div key={projects.title} className="columns portfolio-item">
-					<div className="item-wrap">
-						<a
-							href={projects.url}
-							title={projects.title}
-							target="_blank"
-							rel="noreferrer"
-						>
-							<img
-								alt={projects.title}
-								src={projectImage}
-								style={{ height: "auto" }}
-							/>
-							<div className="overlay">
-								<div className="portfolio-item-meta">
-									<h5>{projects.title}</h5>
-									<p>{projects.description}</p>
-									<br />
-									<p>{projects.category}</p>
-								</div>
-							</div>
-							<div className="link-icon">
-								<i className="fa fa-link"></i>
-							</div>
-						</a>
-					</div>
-				</div>
-			);
-		});
-	}
-
-	return (
-		<section id="portfolio">
-			<div className="row" style={{}}>
-				<div className="twelve columns collapsed">
-					<h1>Check Out Some of My Projects.</h1>
-
-					<div id="portfolio-wrapper" className="portfolio_project">
-						{projects}
-					</div>
-				</div>
-			</div>
-		</section>
-	);
+    return (
+        <section id="portfolio">
+            <div className="row">
+                <div className="twelve columns collapsed">
+                    <h1>Check Out Some of My Projects.</h1>
+                </div>
+            </div>
+            <div id="portfolio-wrapper">
+                {data &&
+                    data.projects.map((projects) => (
+                        <div
+                            key={projects.title}
+                            className="columns portfolio-item"
+                        >
+                            <a
+                                key={projects.title}
+                                href={projects.url}
+                                title={projects.title}
+                                target="_blank"
+                                rel="noreferrer"
+                            >
+                                <div className="item-wrap">
+                                    <img
+                                        alt={projects.title}
+                                        src={
+                                            "images/portfolio/" + projects.image
+                                        }
+                                    />
+                                    <div className="portfolio-box">
+                                        <div className="portfolio-item-meta">
+                                            <h5>{projects.title}</h5>
+                                            <p>{projects.description}</p>
+                                            <p>
+                                                <i>{projects.category}</i>
+                                            </p>
+                                        </div>
+                                    </div>
+                                    <div className="link-icon">
+                                        <i className="fa fa-link"></i>
+                                    </div>
+                                </div>
+                            </a>
+                        </div>
+                    ))}
+                ;
+            </div>
+        </section>
+    );
 };
 
 export default Portfolio;
