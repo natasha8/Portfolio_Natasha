4975370496005327b70d9d224ff562b379c9d42a
natasha8 <nat.parisella@gmail.com>
2022-06-15 09:06:50 +0200

    deploy fix

--- src/Components/Portfolio.js
+++ src/Components/Portfolio.js
@@ -5,7 +5,7 @@ const Portfolio = ({ data }) => {
         <section id="portfolio">
             <div className="row">
                 <div className="twelve columns collapsed">
-                    <h1>Check Out Some of My Projects.</h1>
+                    <h1>Check Out Some of My Projects</h1>
                 </div>
             </div>
             <div id="portfolio-wrapper">
